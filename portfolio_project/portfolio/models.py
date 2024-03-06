95db5330c0bc76a46d95646e9091ff4bbbe622e2
Anishkumar Dhamelia <anishsahilsiya@gmail.com>
2024-03-06 15:26:53 -0500

    Second-Day

--- portfolio_project/portfolio/models.py
+++ portfolio_project/portfolio/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                    .filter(status=Post.Status.PUBLISHED)
+
+
 # Create your models here.
 class Post(models.Model):
 
@@ -22,6 +28,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
