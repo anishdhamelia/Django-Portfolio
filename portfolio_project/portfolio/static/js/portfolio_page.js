f097ab2c6bcb97809ef474ba4c58b7acf3455fc6
Anishkumar Dhamelia <anishsahilsiya@gmail.com>
2024-03-13 19:27:03 -0400

    Seventh-Day

--- portfolio_project/portfolio/static/js/portfolio_page.js
+++ portfolio_project/portfolio/static/js/portfolio_page.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
-    function smallStretchAndCompressAnimation() {
-        // Stretch animation
-        $("#name").animate({
-            fontSize: '5.1vw',
-        }, 800);
-        $("#title").animate({
-            fontSize: '2.6vw',
-        }, 800);
 
-        // Compress animation
-        $("#name").animate({
-            fontSize: '5vw',
-        }, 800);
-        $("#title").animate({
-            fontSize: '2.5vw',
-        }, 800);
-    }
+    // Animate the First Page
+    $("#name").waypoint(function(){
+        $("#name").addClass("animate__animated animate__fadeInDown animate__slow").css('opacity', 1);
+        $("#title").addClass("animate__animated animate__fadeInDown animate__slow").css('opacity', 1);
+    }, {offset: "20vh"});
 
-    setInterval(smallStretchAndCompressAnimation, 10000);
+    $("#name").waypoint(function(){
+        $("#name").removeClass("animate__animated animate__fadeInDown animate__slow").css('opacity', 0);
+        $("#title").removeClass("animate__animated animate__fadeInDown animate__slow").css('opacity', 0);
+    }, {offset: "-20vh"});
+
+    // Animate the Second Page
+    $(".container-content-2").waypoint(function(){
+        $(".container-content-2").addClass("animate__animated animate__fadeInLeft animate__slow");
+    }, {offset: "-20vh"});
+
+
+    $(".education-box").click(function() {
+            var imgSrc = $(this).find("img").attr("src");
+            $("#popup-image").attr("src", imgSrc);
+            $("#popup-container").fadeIn();
+        });
+
+    // Close the popup when the close button or overlay container is clicked
+    $("#close, #popup-container").click(function(event) {
+        if (event.target == this) {
+            $("#popup-container").fadeOut();
+        }
+    });
+
 });
