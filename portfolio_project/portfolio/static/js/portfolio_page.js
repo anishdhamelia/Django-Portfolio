108b31bfcec0dab0995150855f55d638edea673c
Anishkumar Dhamelia <anishsahilsiya@gmail.com>
2024-03-14 19:20:28 -0400

    Eighth-Day

--- portfolio_project/portfolio/static/js/portfolio_page.js
+++ portfolio_project/portfolio/static/js/portfolio_page.js
@@ -13,21 +13,38 @@ $(document).ready(function() {
 
     // Animate the Second Page
     $(".container-content-2").waypoint(function(){
-        $(".container-content-2").addClass("animate__animated animate__fadeInLeft animate__slow");
-    }, {offset: "-20vh"});
+        $(".container-content-2").css('opacity', 0.9);
+        $("#about").addClass("animate__animated animate__fadeInLeft animate__slow");
+        $("#education").addClass("animate__animated animate__fadeInRight animate__slow");
+    }, {offset: "20%"});
+
 
 
     $(".education-box").click(function() {
             var imgSrc = $(this).find("img").attr("src");
             $("#popup-image").attr("src", imgSrc);
+
+            var imgAlt = $(this).find("img").attr("alt");
+
+            if (imgAlt === "master_degree.png") {
+                $("#popup-university").text("University: Stevens Institute of Technology");
+                $("#popup-gpa").text("GPA: 3.801");
+                $("#popup-year").text("Year: 2024");
+            }
+            else {
+                $("#popup-university").text("University: D Y Patil College of Engineering");
+                $("#popup-gpa").text("GPA: 3.18");
+                $("#popup-year").text("Year: 2020");
+            }
             $("#popup-container").fadeIn();
         });
 
-    // Close the popup when the close button or overlay container is clicked
-    $("#close, #popup-container").click(function(event) {
-        if (event.target == this) {
-            $("#popup-container").fadeOut();
-        }
+    $(document).click(function(event) {
+    // Check if the clicked element is not a descendant of the popup container
+    if (!$(event.target).closest("#popup-container").length && !$(event.target).closest(".education-box").length) {
+        $("#popup-container").fadeOut();
+    }
     });
+
 
 });
