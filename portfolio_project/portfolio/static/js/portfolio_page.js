89750f6b04d734d5e48338fb1558344121beff71
Anishkumar Dhamelia <anishsahilsiya@gmail.com>
2024-03-15 18:53:22 -0400

    Ninth-Day

--- portfolio_project/portfolio/static/js/portfolio_page.js
+++ portfolio_project/portfolio/static/js/portfolio_page.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
     // Animate the Second Page
     $(".container-content-2").waypoint(function(){
-        $(".container-content-2").css('opacity', 0.9);
+        $(".container-content-2").css('opacity', 1);
         $("#about").addClass("animate__animated animate__fadeInLeft animate__slow");
         $("#education").addClass("animate__animated animate__fadeInRight animate__slow");
     }, {offset: "20%"});
@@ -40,10 +40,29 @@ $(document).ready(function() {
         });
 
     $(document).click(function(event) {
-    // Check if the clicked element is not a descendant of the popup container
     if (!$(event.target).closest("#popup-container").length && !$(event.target).closest(".education-box").length) {
         $("#popup-container").fadeOut();
     }
+    });
+
+    // Animate the Third Page
+    $(".container-content-3").waypoint(function(){
+        setTimeout(function() {
+        $("#content-3-item-1").addClass("animate__animated animate__fadeInUp animate__slow");
+        }, 0);
+        setTimeout(function() {
+            $("#content-3-item-2").addClass("animate__animated animate__fadeInDown animate__slow");
+        }, 600);
+        setTimeout(function() {
+            $("#content-3-item-3").addClass("animate__animated animate__fadeInUp animate__slow");
+        }, 800);
+        setTimeout(function() {
+            $("#content-3-item-4").addClass("animate__animated animate__fadeInDown animate__slow");
+        }, 1000);
+        setTimeout(function() {
+            $("#content-3-item-5").addClass("animate__animated animate__fadeInUp animate__slow");
+        }, 1200);
+
     });
 
 
